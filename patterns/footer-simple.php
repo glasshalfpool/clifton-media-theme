545065282098ac3109251029d4e1e197e2532166
glasshalfpool <glasshalfpool@gmail.com>
2024-10-23 14:11:03 +0100

    Wealth DFM updates

---
+++ patterns/footer-simple.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: Simple Footer with text, links.
+ * Slug: cliftonmedia/footer-simple
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Footer simple"},"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull"><!-- wp:spacer {"height":"var:preset|spacing|large"} -->
+<div style="height:var(--wp--preset--spacing--large)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:site-logo {"width":200,"align":"center","className":"wealthdfm-logo"} /-->
+
+<!-- wp:navigation {"ref":5,"layout":{"type":"flex","justifyContent":"center"}} /-->
+
+<!-- wp:social-links {"iconColor":"primary","iconColorValue":"#198ace","size":"has-large-icon-size","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links has-large-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"service":"x"} /-->
+
+<!-- wp:social-link {"service":"facebook"} /-->
+
+<!-- wp:social-link {"service":"linkedin"} /-->
+
+<!-- wp:social-link {"service":"instagram"} /--></ul>
+<!-- /wp:social-links -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size">© 2024 All rights reserved to Wealth DFM</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
